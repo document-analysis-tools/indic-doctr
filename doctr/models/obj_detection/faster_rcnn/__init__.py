a6072a60e71121e57391feb9ad245f6bc15125ae
kasuba-badri-vishal <kasubabadri@gmail.com>
2022-09-08 06:47:35 +0530

    Changed default execution for Pytorch

--- doctr/models/obj_detection/faster_rcnn/__init__.py
+++ doctr/models/obj_detection/faster_rcnn/__init__.py
@@ -1,4 +1,6 @@
 from doctr.file_utils import is_tf_available, is_torch_available
 
-if not is_tf_available() and is_torch_available():
-    from .pytorch import *
+if is_torch_available():
+    from .pytorch import *  # type: ignore[misc]
+elif is_tf_available():
+    from .tensorflow import *
--- doctr/transforms/modules/__init__.py
+++ doctr/transforms/modules/__init__.py
@@ -1,8 +1,8 @@
 from doctr.file_utils import is_tf_available, is_torch_available
-
 from .base import *
 
-if is_tf_available():
-    from .tensorflow import *
-elif is_torch_available():
+if is_torch_available():
     from .pytorch import *  # type: ignore[misc]
+elif is_tf_available():
+    from .tensorflow import *
+
